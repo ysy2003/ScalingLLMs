70208c41a1004b5285eb605c14068bf811002380
ysy2003 <yushuyang2003@gmail.com>
2025-11-05 13:52:59 -0500

    dataset

--- dataset.py
+++ dataset.py
@@ -1,5 +1,17 @@
-from datasets import load_dataset
+# from datasets import load_dataset
 
-ds = load_dataset("SALT-NLP/Design2Code-hf", split="train")
-print(ds)
-print(ds[0])
+# ds = load_dataset("SALT-NLP/Design2Code-hf", split="train")
+# print(ds)
+# print(ds[0])
+
+from huggingface_hub import snapshot_download
+
+local_dir = "Design2Code"  # 你想存的本地目录名
+
+snapshot_download(
+    repo_id="SALT-NLP/Design2Code",
+    repo_type="dataset",
+    local_dir=local_dir,
+    local_dir_use_symlinks=False,  # 推荐关掉软链接，方便后面随便移动/打包
+)
+print("Dataset downloaded to", local_dir)
