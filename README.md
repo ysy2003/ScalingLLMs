1a39e02ab1573e95723387a24a4750927933474c
ysy2003 <yushuyang2003@gmail.com>
2025-11-06 12:18:50 -0500

    dataset_update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ScalingLLMs
+
+*Dataset*
+- Design2Code
+- Web2Code
+- Pix2Code
+- Chart2Code: only in google cloud
+- Plotbench
+- WebSight
--- dataset.py
+++ dataset.py
@@ -1,17 +1,53 @@
+# Design2Code
+from datasets import load_dataset
+
+ds = load_dataset("SALT-NLP/Design2Code-hf", split="train")
+print(ds)
+print(ds[0])
+
+# Web2Code
+from datasets import load_dataset
+
+ds = load_dataset("MBZUAI/Web2Code")
+
+print(ds)
+print(ds["train"][0])
+
+# Pix2Code
+from datasets import load_dataset
+
+ds = load_dataset("N0zomu/pix2code-data")
+print(ds)
+print(ds["train"][0])
+
+# WebSight
 # from datasets import load_dataset
 
-# ds = load_dataset("SALT-NLP/Design2Code-hf", split="train")
+# ds = load_dataset("HuggingFaceM4/WebSight", "v0.2")
 # print(ds)
-# print(ds[0])
+# print(ds["train"][0])
 
-from huggingface_hub import snapshot_download
+# Plotbench
+from datasets import load_dataset
 
-local_dir = "Design2Code"  # 你想存的本地目录名
+ds = load_dataset("JetBrains-Research/PandasPlotBench")
+print(ds)
 
-snapshot_download(
-    repo_id="SALT-NLP/Design2Code",
-    repo_type="dataset",
-    local_dir=local_dir,
-    local_dir_use_symlinks=False,  # 推荐关掉软链接，方便后面随便移动/打包
-)
-print("Dataset downloaded to", local_dir)
+# Chart2Code
+# download and upload dataset locally
+
+
+
+# Download 
+
+# from huggingface_hub import snapshot_download
+
+# local_dir = "Design2Code"  # 你想存的本地目录名
+
+# snapshot_download(
+#     repo_id="SALT-NLP/Design2Code",
+#     repo_type="dataset",
+#     local_dir=local_dir,
+#     local_dir_use_symlinks=False,  # 推荐关掉软链接，方便后面随便移动/打包
+# )
+# print("Dataset downloaded to", local_dir)
